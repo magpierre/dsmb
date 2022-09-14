307578763228afdf99becb9901d3981a101466a9
Magnus Pierre <magnus.pierre@icloud.com>
2022-09-14 18:45:32 +0200

    Class borrowed from git-tui
    https://github.com/ArthurSonzogni/git-tui

---
+++ src/Scroller.h
@@ -0,0 +1,15 @@
+#ifndef SCROLLER_H
+#define SCROLLER_H
+
+#include "ftxui/component/component.hpp"
+
+#include "ftxui/component/component_base.hpp"  // for Component
+
+namespace ftxui {
+Component Scroller(Component child);
+}
+#endif
+
+// Copyright 2021 Arthur Sonzogni. All rights reserved.
+// original source: https://github.com/ArthurSonzogni/git-tui
+// git-tui is under MIT-license: https://github.com/ArthurSonzogni/git-tui/blob/master/LICENSE
